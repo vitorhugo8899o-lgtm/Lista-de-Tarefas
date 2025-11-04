8fcce769b8aae1eed2fc11b91939a40b5fee627b
VITOR HUGO RODRIGUES DE LINS <vitorhugo8899o@gmail.com>
2025-11-04 20:16:25 -0300

    Mudando o tipo do ID

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ cliente._carregar_tarefas() #verificando se existe alguma lista
 cliente.adicionar_tarefa('Comprar café') #adiconando a primeira tarefa
 cliente.listar_tarefas() # listando todas as tarefas
 cliente.salvar_tarefa() #salvando a tarefa
-cliente.marcar_concluida(1) #passar o id como str
+cliente.marcar_concluida('1') #passar o id como str
 cliente.remover_tarefa('1') #passar o id como str
